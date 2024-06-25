c3847669945e031c80d29bb19dffdde790ca9755
Kelvin <muhammadfalahalfarisi@gmail.com>
2024-06-25 12:20:12 +0800

    melanjutkan tutorial

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "Blog Dashboard",
+  }
+}
+
 export default function Dashboard() {
   return (
     <div>
--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+type Props = {
+  params: {
+     productId: string;
+  };
+};
 
-export default function Product( {params}: any ) {
+
+export default function Product( {params}: Props ) {
   return (
     <div>
         <h1>Welcome To Product {params.productId}</h1>
