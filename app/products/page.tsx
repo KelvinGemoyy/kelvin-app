94f2adb60cc388afe583da3c7ca4c94b82ad8e02
Kelvin <muhammadfalahalfarisi@gmail.com>
2024-06-26 09:22:31 +0800

    add Link component to learn fundamental

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Product() {
+  const productId = 70;
   return (
     <div>
+      <Link href="/">Back</Link>
         <h1>Welcome To Product Page</h1>
         <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
+           <Link href={`/products/1`}><li>Product 1</li></Link>
+           <Link href={`/products/2`}><li>Product 2</li></Link>
+           <Link href={`/products/3`}><li>Product 3</li></Link>
+           <Link href={`/products/${productId}`}><li>Product {productId}</li></Link>
         </ul>
     </div>
   )
